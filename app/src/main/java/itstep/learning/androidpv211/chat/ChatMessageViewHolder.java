8168ef6e7cda55e343ee599e4e08233b3bcfca61
Kosto_xj46 <Kosto_xj46@student.itstep.org>
2025-04-24 18:00:21 +0300

    HW9 - days counter

--- app/src/main/java/itstep/learning/androidpv211/chat/ChatMessageViewHolder.java
+++ app/src/main/java/itstep/learning/androidpv211/chat/ChatMessageViewHolder.java
@@ -7,6 +7,8 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 import itstep.learning.androidpv211.R;
@@ -15,6 +17,8 @@ import itstep.learning.androidpv211.orm.ChatMessage;
 public class ChatMessageViewHolder extends RecyclerView.ViewHolder {
     public static final SimpleDateFormat momentFormat=
             new SimpleDateFormat("dd.MM HH:mm", Locale.ROOT);
+    public static final SimpleDateFormat timeOnly =
+            new SimpleDateFormat("HH:mm", Locale.ROOT);
     private ChatMessage chatMessage;
     private final TextView tvAuthor;
     private final TextView tvText;
@@ -31,6 +35,30 @@ public class ChatMessageViewHolder extends RecyclerView.ViewHolder {
         this.chatMessage = chatMessage;
         tvAuthor.setText(this.chatMessage.getAuthor());
         tvText.setText(this.chatMessage.getText());
-        tvMoment.setText(momentFormat.format(this.chatMessage.getMoment()));
+        Date msgDate = this.chatMessage.getMoment();
+        Calendar now = Calendar.getInstance();
+        Calendar msgCalendar = Calendar.getInstance();
+        msgCalendar.setTime(msgDate);
+
+        now.set(Calendar.HOUR_OF_DAY,0);
+        now.set(Calendar.MINUTE,0);
+        now.set(Calendar.SECOND,0);
+        now.set(Calendar.MILLISECOND,0);
+        msgCalendar.set(Calendar.HOUR_OF_DAY,0);
+        msgCalendar.set(Calendar.MINUTE,0);
+        msgCalendar.set(Calendar.SECOND,0);
+        msgCalendar.set(Calendar.MILLISECOND,0);
+
+        long difDays=(now.getTimeInMillis()-msgCalendar.getTimeInMillis())/(1000*3600*24);
+        if(difDays==0){
+            tvMoment.setText("Today "+timeOnly.format(msgDate));
+        } else if (difDays==1) {
+            tvMoment.setText("Yesterday "+timeOnly.format(msgDate));
+        } else if (difDays<=7) {
+            tvMoment.setText(difDays+" days ago "+timeOnly.format(msgDate));
+        } else {
+            tvMoment.setText(momentFormat.format(msgDate));
+        }
+
     }
 }
